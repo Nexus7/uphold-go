3cd416f98f57b22cbaf247c4b5f88c8e0d507c95
Rick Behl <rickbehl@gmail.com>
2024-08-04 10:25:32 +0100

    Add package for environment vars and add comments

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+// Import the required packages
 import (
 	"encoding/json"
 	"fmt"
@@ -8,25 +9,32 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
+// BalanceDataResponse struct
 type BalanceDataResponse struct {
 	Balance string `json:"balance"`
 }
 
 // Retrieve Balance data from Alephium API
 func retrieveBalanceData(acct string) {
-	url := "https://node.mainnet.alephium.org/addresses/" + acct + "/balance"
+	// Get the Alephium Mainnet URL from the environment variable
+	url := os.Getenv("ALEPHIUM_MAINNET_URL") + acct + "/balance"
 	method := "GET"
 
+	// Create a new HTTP client
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, nil)
 
+	// Check for errors
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// Send the request
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
@@ -34,28 +42,39 @@ func retrieveBalanceData(acct string) {
 	}
 	defer res.Body.Close()
 
+	// Read the response body
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// Unmarshal the JSON response
 	var r = new(BalanceDataResponse)
 	err2 := json.Unmarshal(body, &r)
 	if err2 != nil {
 		log.Fatal(err2)
 	}
 
+	// Convert the balance to a float
 	balance, err := strconv.ParseFloat(r.Balance, 64)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Convert the balance to Alephium and print it
 	balance /= 1e18
 	fmt.Println("Balance: ", balance)
 }
 
 func main() {
+	// Load the .env file
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	// Get the account balance from command line argument
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide an account balance to query")
@@ -63,5 +82,6 @@ func main() {
 	}
 	acct := os.Args[1]
 
+	// Retrieve the balance data
 	retrieveBalanceData(acct)
 }
